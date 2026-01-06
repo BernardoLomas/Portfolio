f6cf7b596b6bf59aeeabb2f2ce93bdcc81773733
BernardoLomas <bernardo.lomasb@gmail.com>
2026-01-05 23:49:43 -0300

    feat(translating): translating the softskills texts to english

--- portfolio-bernardolomas/src/data/softskills.ts
+++ portfolio-bernardolomas/src/data/softskills.ts
@@ -5,8 +5,18 @@ export type SoftSkills = {
 
 export const softSkills: SoftSkills[] = [
   {
-    title: "Inglês Fluente",
+    title: "Global Communication (English)",
     description:
-      "Ingles fluente",
+      "Advanced proficiency, enabling seamless collaboration with international squads and stakeholders across diverse cultural and professional environments.",
+  },
+  {
+    title: "Adaptability & Knowledge Sharing",
+    description: 
+      "Quick to master new stacks and tools while fostering a collaborative culture by sharing insights and mentoring team members, enabling a productive teamwork"
+  },
+  {
+    title: "Collaborative Mindset",
+    description: 
+      "Passionate about cross-functional collaboration. I believe that teamwork and open communication are the foundations of building exceptional software."
   }
 ]
