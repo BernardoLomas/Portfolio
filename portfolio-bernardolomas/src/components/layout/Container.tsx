3b8a2b63399404eb69b7e677d2d94fcc1de0c53e
BernardoLomas <bernardo.lomasb@gmail.com>
2026-01-10 20:38:27 -0300

    feat(responsiveness): Ultra wide responsiveness in Container section

--- portfolio-bernardolomas/src/components/layout/Container.tsx
+++ portfolio-bernardolomas/src/components/layout/Container.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 export default function Container({ children, className }: Props){
     return (
-        <div className={`mx-auto w-full max-w-7xl px-6 sm:px-8 ${className ?? ""}`}>
+        <div className={`mx-auto w-full max-w-7xl px-6 sm:px-6 lg:px-8 xl:max-w-[1440px] ${className ?? ""}`}>
             {children}
         </div>
     )
