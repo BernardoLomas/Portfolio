d5269409073b54f3389a0603ed61d32103c16a1f
BernardoLomas <bernardo.lomasb@gmail.com>
2026-01-11 14:13:35 -0300

    refactor(about): refactor texts in about section

--- portfolio-bernardolomas/src/pages/About/Bio.tsx
+++ portfolio-bernardolomas/src/pages/About/Bio.tsx
@@ -1,39 +1,37 @@
 export default function Bio() {
   return (
-    <section className="max-w-3xl space-y-12 text-zinc-300">
-      <header className="space-y-2">
-        <h1 className="text-4xl font-bold tracking-tight">
-          Turning logic into experiences:
-        </h1>
-        <span className="block text-4xl font-bold text-emerald-300">
-          developing software and people
-        </span>
-      </header>
+<section className="max-w-3xl space-y-10 text-zinc-300">
+  <header className="space-y-2">
+    <h1 className="text-4xl font-bold tracking-tight">
+      Turning <span className="text-emerald-400 font-bold">logic</span> into<span className="text-emerald-400 font-bold"> experiences:</span>{" "}developing software and people
+    </h1>
+  </header>
 
-      <div className="space-y-4">
-        <h2 className="text-3xl font-bold tracking-tight text-white">
-          About
-        </h2>
+  <div className="space-y-5 text-justify">
+    <p>
+      I'm a <span className="text-emerald-400 font-bold">full stack software developer</span> with a
+      strong background in both technology and education, combining
+      technical problem-solving with clear communication and
+      purpose-driven work.
+    </p>
 
-        <p>
-          I am a full stack software developer with experience in both
-          technology and education, combining technical problem-solving
-          with clear communication and purpose-driven work.
-        </p>
+    <p>
+      Over the past year, I've been focused on building{" "}
+      <span className="text-emerald-400 font-bold">real-world academic and personal projects</span>,
+      while also teaching English to more than{" "}
+      <span className="text-emerald-400 font-bold">20 students</span>, consistently
+      improving their performance through personalized learning
+      strategies.
+    </p>
 
-        <p>
-          Over the past year, I have been building results through academic
-          and personal projects, as well as teaching English to more than
-          <strong className="text-white"> 20 students</strong>, consistently
-          improving their performance through personalized learning
-          strategies.
-        </p>
+    <p>
+      I'm driven by the challenge of building functional,
+      well-structured systems and sharing knowledge in a way that creates
+      <span className="text-emerald-400 font-bold"> measurable impact</span>  both in
+      software and in people.
+    </p>
+  </div>
+</section>
 
-        <p>
-          My focus is on building functional, well-structured systems and
-          sharing knowledge in a way that generates real impact.
-        </p>
-      </div>
-    </section>
   );
 }
