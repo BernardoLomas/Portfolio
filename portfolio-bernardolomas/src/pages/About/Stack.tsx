5207aa61b3aa9c7f3144241c24424207a62df06c
BernardoLomas <bernardo.lomasb@gmail.com>
2026-01-19 15:52:09 -0300

    feat(i18n): added on about section, stack

--- portfolio-bernardolomas/src/pages/About/Stack.tsx
+++ portfolio-bernardolomas/src/pages/About/Stack.tsx
@@ -1,14 +1,17 @@
 import SectionHeader from "../../components/ui/SectionHeader";
 import StackGroup from "./StackGroup";
 import { main, improving, learning } from "../../data/stacks";
+import { useTranslation } from "react-i18next";
 
 export default function Stack() {
+  const { t } = useTranslation();
+
   return (
     <div className="space-y-20">
       <div>
         <SectionHeader
-          title="Main"
-          subtitle="Technologies that i work with confidently in real projects"
+          title={t("stack.groups.main.title")}
+          subtitle={t("stack.groups.main.subtitle")}
           align="center"
         />
         <StackGroup items={main} />
@@ -16,8 +19,8 @@ export default function Stack() {
 
       <div>
         <SectionHeader
-          title="Improving"
-          subtitle="Technologies i actively use and deepen my knowledge"
+          title={t("stack.groups.improving.title")}
+          subtitle={t("stack.groups.improving.subtitle")}
           align="center"
         />
         <StackGroup items={improving} />
@@ -25,8 +28,8 @@ export default function Stack() {
 
       <div>
         <SectionHeader
-          title="Learning"
-          subtitle="Technologies that i plan to explore in the near future"
+          title={t("stack.groups.learning.title")}
+          subtitle={t("stack.groups.learning.subtitle")}
           align="center"
         />
         <StackGroup items={learning} />
